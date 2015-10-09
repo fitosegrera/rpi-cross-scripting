8afb6ac7980f32256d4302a7a7a705063f031084
fitosegrera <fitosegrera@gmail.com>
2015-10-09 23:54:54 +0800

    uncomplete

--- rpi-cross-scripting.py
+++ rpi-cross-scripting.py
@@ -1,5 +1,5 @@
 import sublime, sublime_plugin
-import subprocess
+from subprocess import PIPE,Popen
 import json
 import os
 
@@ -7,11 +7,15 @@ print("plugin loaded")
 
 class Rpi(sublime_plugin.TextCommand):
 	def run(self, edit):
-		script_dir = os.path.dirname(__file__)
-		rel_path = "rpi-config.json"
-		abs_file_path = os.path.join(script_dir, rel_path)
-		with open(abs_file_path, 'r') as f:
-			jsonData = json.load(f)
 
-		commandToExecute = "scp " + jsonData["origin"] + " " + jsonData["username"] + "@" + jsonData["ip"] + jsonData["target"]
-		subprocess.call(commandToExecute, shell=True)
+		# script_dir = os.path.dirname(__file__)
+		# rel_path = "rpi-config.json"
+		# abs_file_path = os.path.join(script_dir, rel_path)
+		# with open(abs_file_path, 'r') as f:
+		# 	jsonData = json.load(f)
+
+		# commandToExecute = "scp " + jsonData["origin"] + " " + jsonData["username"] + "@" + jsonData["ip"] + jsonData["target"]
+		# subprocess.call(commandToExecute, shell=True)
+		#subprocess.call('sshpass -pFito_1015! ssh -o StrictHostKeyChecking=no root@192.168.1.106 ls /shares', shell=True)
+		proc = Popen(['sshpass', '-p', 'Fito_1015!', 'ssh', '-o', 'StrictHostKeyChecking=no', 'root@192.168.1.106', 'ls', '/shares'], stdout=PIPE)
+		print(proc.communicate())
